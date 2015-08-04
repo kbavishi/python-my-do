4cda52e242dc99a15542aee32732e5b148046f5a
Karan Bavishi <karan.bavishi90@gmail.com>
2015-08-04 19:36:44 +0530

    Adding ability to take due_date as a parameter in the POST request

--- MyDoServer.py
+++ MyDoServer.py
@@ -3,6 +3,7 @@
 import datetime
 import json
 import os
+import time
 from flask import Flask, request
 from anydo.api import AnyDoAPI
 
@@ -19,19 +20,36 @@ def hello():
 @app.route( "/add_task", methods=[ 'POST' ] )
 def addTask():
     assert request.method == 'POST'
+
+    # Verify that key arguments were provided
     if not request.json.get( "key" ):
-      return "key not provided"
+      return "Key not provided"
     if not validLogin( request.json.get( "key" ) ):
       return "Invalid login"
     if not request.json.get( "task_name" ):
        return "Task name not provided"
+
     text = "New task being added!\n"
+
+    # Login with the Any.Do credentials
     anyDoUsername = os.environ.get( "ANY_DO_USERNAME", "" )
     anyDoPassword = os.environ.get( "ANY_DO_PASSWORD", "" )
     api = AnyDoAPI( username=anyDoUsername, password=anyDoPassword )
-    # Assume due date of tomorrow for now.
-    tomorrow = datetime.datetime.today() + datetime.timedelta( hours=+24 )
-    api.create_new_task( request.json.get( "task_name" ), due_day=tomorrow )
+
+    if not request.json.get( "due_date" ):
+      # Assume a default due date of tomorrow, if nothing is provided.
+      due_day = datetime.datetime.today() + datetime.timedelta( hours=+24 )
+    else:
+      # Expected to be in a certain format. Eg: November 21, 2006 at 04:30PM
+      dateFormat = "%B %d, %Y at %I:%M%p"
+      # Convert to struct_time
+      date_struct_time = time.strptime( request.json[ "due_date" ], dateFormat )
+      # Convert from struct_time to datetime
+      due_day = datetime.datetime.fromtimestamp( time.mktime( date_struct_time ) )
+
+    # Create the task
+    api.create_new_task( request.json.get( "task_name" ), due_day=due_day )
+
     return text
 
 if __name__ == "__main__":
