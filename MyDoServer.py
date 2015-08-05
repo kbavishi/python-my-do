f7f0e563b4b530dcdd258c34fd24d24680160fc1
Karan Bavishi <karan.bavishi90@gmail.com>
2015-08-05 15:55:50 +0530

    Fixing indentation issues

--- MyDoServer.py
+++ MyDoServer.py
@@ -24,11 +24,11 @@ def addTask():
 
     # Verify that key arguments were provided
     if not request.json.get( "key" ):
-      return DisplayStrings.KEY_NOT_PROVIDED_ERROR
+        return DisplayStrings.KEY_NOT_PROVIDED_ERROR
     if not validLogin( request.json.get( "key" ) ):
-      return DisplayStrings.INVALID_KEY_ERROR
+        return DisplayStrings.INVALID_KEY_ERROR
     if not request.json.get( "task_name" ):
-       return DisplayStrings.NO_TASK_NAME_ERROR
+        return DisplayStrings.NO_TASK_NAME_ERROR
 
     # Login with the Any.Do credentials
     anyDoUsername = os.environ.get( "ANY_DO_USERNAME", "" )
@@ -36,25 +36,24 @@ def addTask():
     api = AnyDoAPI( username=anyDoUsername, password=anyDoPassword )
 
     if not request.json.get( "due_date" ):
-      # Assume a default due date of tomorrow, if nothing is provided.
-      due_day = datetime.datetime.today() + datetime.timedelta( hours=+24 )
+        # Assume a default due date of tomorrow, if nothing is provided.
+        due_day = datetime.datetime.today() + datetime.timedelta( hours=+24 )
     else:
-      # Expected to be in a certain format. Eg: November 21, 2006 at 04:30PM
-      dateFormat = "%B %d, %Y at %I:%M%p"
-      # Convert to struct_time
-      date_struct_time = time.strptime( request.json[ "due_date" ], dateFormat )
-      # Convert from struct_time to datetime
-      due_day = datetime.datetime.fromtimestamp( time.mktime( date_struct_time ) )
+        # Expected to be in a certain format. Eg: November 21, 2006 at 04:30PM
+        dateFormat = "%B %d, %Y at %I:%M%p"
+        # Convert to struct_time
+        date_struct_time = time.strptime( request.json[ "due_date" ], dateFormat )
+        # Convert from struct_time to datetime
+        due_day = datetime.datetime.fromtimestamp( time.mktime( date_struct_time ) )
 
     # Create the task
     try:
-      api.create_new_task( request.json.get( "task_name" ), due_day=due_day )
-      return DisplayStrings.TASK_ADD_SUCCESSFUL
+        api.create_new_task( request.json.get( "task_name" ), due_day=due_day )
+        return DisplayStrings.TASK_ADD_SUCCESSFUL
     except:
-      # Failed for some reason
-      return DisplayStrings.TASK_ADD_FAILED
+        # Failed for some reason
+        return DisplayStrings.TASK_ADD_FAILED
 
 if __name__ == "__main__":
     port = int( os.environ.get( "PORT", 5000 ) )
-    app.debug = True
     app.run( host='0.0.0.0', port=port )
